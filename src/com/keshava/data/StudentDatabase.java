4fae67d942b5f744889a43cdef7bc96f89e8bdf9
adithyakeshav <adithyakak0@gmail.com>
2020-09-01 10:48:00 +0530

    Student Class and Database classes

---
+++ src/com/keshava/data/StudentDatabase.java
@@ -0,0 +1,18 @@
+package com.keshava.data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentDatabase {
+
+    public static List<Student> getAllStudents() {
+        Student student1 = new Student("Adithya", 2, 3.6, "male", Arrays.asList("Chess", "Volleyball", "Cricket"));
+        Student student2 = new Student("Keshava", 2, 4.0, "male", Arrays.asList("Kabaddi", "Football", "Singing"));
+        Student student3 = new Student("Ramana", 3, 4.2, "male", Arrays.asList("Kho-Kho", "Volleyball", "Dance"));
+        Student student4 = new Student("Ajith", 3, 5.0, "male", Arrays.asList("Snooker", "Fitness", "Drama"));
+        Student student5 = new Student("Abhay", 2, 4.6, "male", Arrays.asList("chess", "Kabaddi", "Dance"));
+    
+        return Arrays.asList(student1, student2, student3, student4, student5);
+    }
+    
+}
